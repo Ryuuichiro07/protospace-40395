0ce88da07a5eaad020f8464ef717d503075a7fd3
Ryuuichiro07 <159006757+Ryuuichiro07@users.noreply.github.com>
2024-02-25 23:08:54 +0900

    ユーザー管理機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'prototypes/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "prototypes#index"
+  
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
